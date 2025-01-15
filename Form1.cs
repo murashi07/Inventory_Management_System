778a7745dea9c27dd8b1bbff3307f739fb60686e
murashi07 <yvanmurashi@gmail.com>
2025-01-16 00:11:23 +0200

    design

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace Inventory_mgt_system
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
